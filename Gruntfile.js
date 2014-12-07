bf4fe9b6e9d7b0c8d542d23f58c9a5fff741c47f
Felix Boamah <dotfelixb@gmail.com>
2014-12-07 16:42:36 +0000

    add watch task to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,30 @@
 module.exports = function ( grunt ) {
 	/* configure grunt */
 
-	var srcDir  = 'lib/**/*.js',
-		destDir = 'dist/',
-		outFile = destDir + '<%= pkg.name %>.js';
+	var srcDir 		= 'lib/',
+		srcFiles 	= srcDir + '**/*.js',
+		destDir 	= 'dist/',
+		outFile 	= destDir + '<%= pkg.name %>.js',
+		implDir 	= 'impl/',
+		implFiles 	= implDir + '**/*';
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			js: {
+				files: [ srcFiles ],
+				tasks: [ 'jshint:all' , 'browserify:main' ]
+			},
+			impl: {
+				files: [ implFiles ],
+				tasks: [ 'jshint:all' , 'browserify:main' ],
+				options: {
+					livereload: true
+				}
+			}
+		},
 		jshint: {
-			all: ['Gruntfile.js', srcDir ],
+			all: ['Gruntfile.js', srcFiles ],
 			options: {
 				jshintrc: '.jshintrc',
 				reporter: require('jshint-stylish')
@@ -18,7 +34,7 @@ module.exports = function ( grunt ) {
 		},
 		browserify: {
 			main: {
-				src: [ srcDir ],
+				src: [ srcFiles ],
 				dest: outFile,
 				options: {
 					debug: true
@@ -38,6 +54,7 @@ module.exports = function ( grunt ) {
 	});
 
 	/* load plugins */
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
